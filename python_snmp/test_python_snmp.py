f44a8a156b978b26966c2fde3391ce46c6ec79ec
Rob Day <Robert.Day@metaswitch.com>
2015-07-09 09:38:25 +0000

    Wrap in proper Python class

--- python_snmp/test_python_snmp.py
+++ python_snmp/test_python_snmp.py
@@ -1,8 +1,19 @@
 from _pysnmp import ffi, lib
 
+
+class CounterTable(object):
+    def __init__(self, name, oid):
+        self.tbl = lib.create_counter_table(name, oid)
+
+    def __del__(self):
+        if lib is not None:
+            lib.delete_counter_table(self.tbl)
+
+    def increment(self):
+        lib.increment_counter_table(self.tbl)
+
 lib.snmp_setup("rkd")
-tbl = lib.create_counter_table("rkd-tbl", ".1.2.3.4")
-lib.increment_counter_table(tbl)
-lib.delete_counter_table(tbl)
+t = CounterTable("rkd-tbl", ".1.2.3.4")
+t.increment()
 lib.snmp_terminate("rkd")
 
